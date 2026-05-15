1173fb49fbac2c49c6bcdadb5ff87918eb0e1c11
lseaton <73501741+lseaton@users.noreply.github.com>
2026-05-15 16:56:30 -0700

    Remove buggy auto looping slideshow feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //plan: two images, curr_slide and next_slide. next slide is off screen.
 // at designated intervals, apply translation to slides and end with popping up a new next slide off screen
 // cycle
-
+/*
 let slides = [
 	"./assets/carousel/img-1.jpg",
 	"./assets/carousel/img-2.jpg",
@@ -15,12 +15,12 @@ let carousel = document.querySelector(".slides");
 
 let slide = document.querySelector(".slides img");
 
+
 slide.addEventListener("animationend", () => {
 	// JavaScript code to execute after the animation ends
 	console.log("CSS animation finished!");
 	carousel.removeChild(curr_slide);
-	curr_slide = next_slide;
-	next_slide = createSlide(0);
+	next_slide = createSlide(slides.indexOf(curr_slide.getAttribute("src")) + 2);
 	carousel.appendChild(next_slide);
 	// For example, remove the animation class to reset or trigger another action
 	//element.classList.remove('animate-class');
@@ -33,4 +33,4 @@ function createSlide(index) {
 	newSlide.setAttribute("alt", "Image of person skydiving");
 	newSlide.setAttribute("width", "500px");
 	return newSlide;
-}
+}*/
